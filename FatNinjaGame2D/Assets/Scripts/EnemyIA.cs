a200f4c74d2cd7b9b5a0a3a6b23cedf93624005d
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-08 21:29:00 +0300

    Added triggering the enemy

--- FatNinjaGame2D/Assets/Scripts/EnemyIA.cs
+++ FatNinjaGame2D/Assets/Scripts/EnemyIA.cs
@@ -8,6 +8,8 @@ public class EnemyIA : MonoBehaviour
     public Vector2 pos1, pos2;
     public float leftRightSpeed;
     private float oldPosition;
+
+  
     void Start()
     {
         
@@ -16,10 +18,10 @@ public class EnemyIA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        EnemyMove();
+       
     }
 
-    void EnemyMove()
+    public void EnemyMove()
     {
         transform.position = Vector3.Lerp(pos1,pos2,Mathf.PingPong(Time.time * leftRightSpeed,1.0f));
 
@@ -33,4 +35,6 @@ public class EnemyIA : MonoBehaviour
         }
         oldPosition = transform.position.x; 
     }
+
+   
 }
---
+++ FatNinjaGame2D/Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    public float Distance;
+    private Transform Target;
+    public float FallowSpeed;
+    EnemyIA enemyIA;
+    void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        enemyIA =GetComponent<EnemyIA>();
+    }
+
+    
+    void Update()
+    {
+        EnemyActive();
+    }
+
+    void EnemyActive()
+    {
+        RaycastHit2D enemyActive = Physics2D.Raycast(transform.position, transform.right,Distance);
+
+        if (enemyActive.collider != null)
+        {
+            Debug.DrawLine(transform.position,enemyActive.point, Color.red);
+            EnemyFallow();
+        }
+        if (enemyActive.collider == null)
+        {
+            Debug.DrawLine(transform.position, enemyActive.point, Color.green);
+            enemyIA.EnemyMove();
+        }
+    }
+    void EnemyFallow()
+    {
+        Vector3 targetPos = new Vector3(Target.position.x, gameObject.transform.position.y, Target.position.x);
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, FallowSpeed * Time.deltaTime);
+    }
+}
