06d642e68feaf7e9d9d59f99d7d2435d24361af1
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-08 20:53:16 +0300

    Add to Enemy Patroling

---
+++ FatNinjaGame2D/Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class EnemyIA : MonoBehaviour
+{
+    public Vector2 pos1, pos2;
+    public float leftRightSpeed;
+    private float oldPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    void EnemyMove()
+    {
+        transform.position = Vector3.Lerp(pos1,pos2,Mathf.PingPong(Time.time * leftRightSpeed,1.0f));
+
+        if (transform.position.x > oldPosition)
+        {
+            transform.localRotation = Quaternion.Euler(0,0,0);
+        }
+        if (transform.position.x < oldPosition)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        oldPosition = transform.position.x; 
+    }
+}
