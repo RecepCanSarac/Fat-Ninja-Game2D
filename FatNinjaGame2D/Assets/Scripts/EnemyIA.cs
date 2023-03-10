d2329d4a83d75026bc1ba3facd660202747179dd
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-10 17:35:41 +0300

    MeleeEnemyİdle Okay!

--- FatNinjaGame2D/Assets/Scripts/EnemyIA.cs
+++ FatNinjaGame2D/Assets/Scripts/EnemyIA.cs
@@ -6,15 +6,16 @@ using UnityEngine;
 
 public class EnemyIA : MonoBehaviour
 {
-    public  float speed;
+    public float speed;
     public float distance;
     private bool moveinRight = true;
     public Transform groundControl;
-    
+
+    private Animator animator;
   
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,15 +29,39 @@ public class EnemyIA : MonoBehaviour
         transform.Translate(Vector2.right * speed * Time.deltaTime);
         RaycastHit2D ground = Physics2D.Raycast(groundControl.position,Vector2.down, distance);
 
-        if (ground.collider == true)
+        if (ground.collider == false)
         {
-            transform.Rotate(0,0,0);
+            if (moveinRight == true)
+            {
+                StartCoroutine(Timer1());
+            }
+            else
+            {
+                StartCoroutine(Timer2());
+            }
+           
         }
-        else if(ground.collider == false)
-        {
-            transform.Rotate(0, 180, 0);
-        }
+        
     }
 
-   
+   IEnumerator Timer1()
+    {
+        speed = 0;
+        animator.SetBool("Stop",true);
+        yield return new WaitForSeconds(0.7f);
+        animator.SetBool("Stop", false);
+        transform.eulerAngles = new Vector3(0, -180, 0);
+        moveinRight = false;
+        speed = 1;
+    }
+    IEnumerator Timer2()
+    {
+        speed = 0;
+        animator.SetBool("Stop", true);
+        yield return new WaitForSeconds(0.7f);
+        animator.SetBool("Stop", false);
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        moveinRight = true;
+        speed = 1;
+    }
 }
