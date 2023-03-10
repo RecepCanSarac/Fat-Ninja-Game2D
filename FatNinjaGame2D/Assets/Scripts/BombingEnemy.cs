00291f50d1d9a453f207f30fcece88378fe614aa
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-10 20:56:05 +0300

    Bombing Enemy Shoot okay

--- FatNinjaGame2D/Assets/Scripts/BombingEnemy.cs
+++ FatNinjaGame2D/Assets/Scripts/BombingEnemy.cs
@@ -8,6 +8,11 @@ public class BombingEnemy : MonoBehaviour
     public Transform target;
     bool Detected = false;
     Vector2 direction;
+    public GameObject bomb;
+    public Transform bombPoint;
+    public float Force;
+    public float FireRate;
+    private float nextTimeToFire = 0;
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
@@ -49,6 +54,21 @@ public class BombingEnemy : MonoBehaviour
                 Detected = false;
             }
         }
+        if (Detected)
+        {
+            if (Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1 / FireRate;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+       GameObject Bomb =  Instantiate(bomb,bombPoint.position,Quaternion.identity);
+        Bomb.GetComponent<Rigidbody2D>().AddForce(direction * Force);
+        Bomb.GetComponent<Rigidbody2D>().AddForce(transform.up * 150);
     }
 
 }
