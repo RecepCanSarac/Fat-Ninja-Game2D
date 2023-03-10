8543087f93914e8339d3ecfae3084c8aec00f834
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-10 20:38:04 +0300

    trigger circle ok BombıngEnemy

--- FatNinjaGame2D/Assets/Scripts/BombingEnemy.cs
+++ FatNinjaGame2D/Assets/Scripts/BombingEnemy.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class BombingEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float radius = 2f;
+    public Transform target;
+    bool Detected = false;
+    Vector2 direction;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,radius); 
+    }
     void Start()
     {
         
@@ -13,6 +21,34 @@ public class BombingEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TriggerEnemy();
     }
+
+    void TriggerEnemy()
+    {
+        Vector2 targetPos = target.position;
+        direction = targetPos - (Vector2)transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,direction,radius);
+
+        if (hit)
+        {
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                if (Detected == false)
+                {
+                    Debug.Log("iceride");
+                    Detected = true;
+                }
+            }
+        }
+        else
+        {
+            if (Detected == true)
+            {
+                Debug.Log("Diþarida");
+                Detected = false;
+            }
+        }
+    }
+
 }
