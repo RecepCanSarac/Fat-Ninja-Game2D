d8f09f93a9594f0dd4373fa5df1e3d8633e3b757
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-14 18:09:13 +0300

    Turret AreaControl Kodding

--- FatNinjaGame2D/Assets/Scripts/Turret.cs
+++ FatNinjaGame2D/Assets/Scripts/Turret.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     public float Radius;
-
+    public Transform Target;
+    Vector2 Direction;
+    private bool Detected = false;
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
@@ -19,6 +21,32 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TriggerTurret();
+    }
+
+    private void TriggerTurret()
+    {
+        Vector2 newPos = Target.position;
+        Direction = newPos - (Vector2)transform.position;
+
+        RaycastHit2D AreaControl = Physics2D.Raycast(transform.position, Direction,Radius);
+
+        if (AreaControl)
+        {
+            if (AreaControl.collider.gameObject.CompareTag("Player"))
+            {
+                if (Detected == false)
+                {
+                    Detected = true;
+                }
+            }
+            else
+            {
+                if (Detected == true)
+                {
+                    Detected = false;
+                }
+            }
+        }
     }
 }
